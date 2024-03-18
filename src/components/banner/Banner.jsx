5722443649296512526812fed6bb86f18c0207b4
juanchos85 <86373520+juanchos85@users.noreply.github.com>
2024-03-18 11:56:22 -0300

    updates

---
+++ src/components/banner/Banner.jsx
@@ -0,0 +1,18 @@
+
+
+export const Banner = () => {
+   
+  return (
+    <div>
+
+
+<img src="https://res.cloudinary.com/dzux3ynpe/image/upload/v1710649100/happydog/veez7lqfqp79tq2ugpmi.png" alt="" />
+
+
+
+
+
+</div>
+
+  )
+}
